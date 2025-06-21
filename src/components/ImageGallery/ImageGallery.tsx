7b8bcdad3187aaca4a2c3c9b7892766582e337b3
Maksym Drachenko <maks7d@gmail.com>
2025-06-21 19:46:21 +0500

    fix

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -1,18 +1,3 @@
-// import ImageCard from '../ImageCard/ImageCard';
-// import css from './ImageGallery.module.css';
-
-// export default function ImageGallery({ items, onImageClick }) {
-//   return (
-//     <ul className={css.list}>
-//       {items.map((item) => (
-//         <li key={item.id} className={css.item}>
-//           <ImageCard newItem={item} onClick={onImageClick} />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
 import ImageCard from "../ImageCard/ImageCard";
 import css from "./ImageGallery.module.css";
 import { UnsplashImage } from "../../types";
--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,13 +1,3 @@
-// import css from './LoadMoreBtn.module.css';
-
-// export default function LoadMoreBtn({ onClick }) {
-//   return (
-//     <button className={css.btn} type="button" onClick={onClick}>
-//       More
-//     </button>
-//   );
-// }
-
 import css from "./LoadMoreBtn.module.css";
 
 interface Props {
